e5df63a5d5cc82ef1f6c8c71b5f07e46fb5a6f74
Alexandria Eicher <alexandriaeicher@gmail.com>
2020-10-23 13:00:41 -0400

    updated main and finished part 2.3

--- main.m
+++ main.m
@@ -1,20 +1,18 @@
-function point2D = main()
+function points2D = main()
+
 load 'Subject4-Session3-Take4_mocapJoints.mat' mocapJoints
-load 'vue2CalibInfo.mat' vue2
-load 'vue4CalibInfo.mat' vue4
 
 %input and parsing of mocap dataset
-for mocapFnum = 1000:1001 %size(mocapJoints, 1)
+for mocapFnum = 1000:1002 %size(mocapJoints, 1)
     conf = mocapJoints(mocapFnum,:,4); %confidence values
     s = sum(conf); %sum should equal 12
     if s == 12
         x = mocapJoints(mocapFnum,:,1); %array of 12 X coordinates
         y = mocapJoints(mocapFnum,:,2); % Y coordinates
         z = mocapJoints(mocapFnum,:,3); % Z coordinates (joint coords)
-        % need to get the camera parameters here to figure out focalLen
-        point2D = project3DpointInto2Dpixel(z, mocapFnum, 1557.8);
+        
+        points2D = project3DpointInto2Dpixel(x, y, z, mocapFnum);
     end
 end
-%input and parsing of camera parameters
 
 end %main
--- project3DpointInto2Dpixel.m
+++ project3DpointInto2Dpixel.m
@@ -1,28 +1,61 @@
-function [newPoint] = project3DpointInto2Dpixel(point, frame, focalLen)
-% This function takes a single 3D point and projects it into a 2D pixel
-% location while ignoring the nonlinear distortion parameters in the 
-% "radial" field.
+function [points2, points4] = project3DpointInto2Dpixel(X, Y, Z, frame)
+    % This function takes a single 3D point and projects it into a 2D pixel
+    % location while ignoring the nonlinear distortion parameters in the 
+    % "radial" field.
+    load 'vue2CalibInfo.mat' vue2
+    load 'vue4CalibInfo.mat' vue4
+    filenamevue2mp4 = 'Subject4-Session3-24form-Full-Take4-Vue2.mp4';
+    filenamevue4mp4 = 'Subject4-Session3-24form-Full-Take4-Vue4.mp4';
 
-X = point(1);
-Y = point(2);
-Z = point(3);
+    % Get Kmat and Pmat from vue
+    Kmat2 = vue2.Kmat;
+    Pmat2 = vue2.Pmat;
+    Kmat4 = vue4.Kmat;
+    Pmat4 = vue4.Pmat;
+    
+    % create a 4x1 matrix using the 3D coordinates
+    UVW = [X;Y;Z;ones(1,12)];
 
-x = focalLen * (X ./ Z);
-y = focalLen * (Y ./ Z);
+    % Get the x, y, and z image points
+    XYZ2 = Kmat2 * Pmat2 * UVW;
+    XYZ4 = Kmat4 * Pmat4 * UVW;
 
-newPoint = [x y focalLen];
+    %initialization of VideoReader for the vue2 video.
+    vue2Video = VideoReader(filenamevue2mp4);
+    vue4Video = VideoReader(filenamevue4mp4);
+    vue2Video.CurrentTime = (frame-1)*(50/100)/vue2Video.FrameRate;
+    vue4Video.CurrentTime = (frame-1)*(50/100)/vue4Video.FrameRate;
+    vid2Frame = readFrame(vue2Video);
+    vid4Frame = readFrame(vue4Video);
+    figure(frame);
+    set(gcf, 'Position',  [100, 100, 1000, 400])
 
-%For verification, visualize your projected 2D joint by overlaying it as
-% a point on the 2D video frame corresponding to the motion capture frame.
+    x2 = zeros(0, 12);
+    y2 = zeros(0, 12);
+    x4 = zeros(0, 12);
+    y4 = zeros(0, 12);
 
-%initialization of VideoReader for the vue video.
-%YOU ONLY NEED TO DO THIS ONCE AT THE BEGINNING
-filenamevue2mp4 = 'Subject4-Session3-24form-Full-Take4-Vue2.mp4';
-vue2video = VideoReader(filenamevue2mp4);
-%now we can read in the video for any mocap frame mocapFnum.
-%the (50/100) factor is here to account for the difference in frame
-%rates between video (50 fps) and mocap (100 fps).
-vue2video.CurrentTime = (frame-1)*(50/100)/vue2video.FrameRate;
-vid2Frame = readFrame(vue2video);
-image(vid2Frame);
+    % divide each x and y by z to get x, y coordinates
+    for i=1:12
+        x2(i) = XYZ2(1,i) ./ XYZ2(3,i);
+        y2(i) = XYZ2(2,i) ./ XYZ2(3,i);
+        x4(i) = XYZ4(1,i) ./ XYZ4(3,i);
+        y4(i) = XYZ4(2,i) ./ XYZ4(3,i);
+    end
+    % plot coordinates on image
+    subplot(1,2,1)
+    image(vid2Frame);
+    axis on;
+    hold on;
+    plot(x2, y2, '.', 'MarkerSize', 15, 'LineWidth', 2);
+    
+    subplot(1,2,2);
+    image(vid4Frame);
+    axis on;
+    hold on;
+    plot(x4, y4, '.', 'MarkerSize', 15, 'LineWidth', 2);
+    
+    % return x,y coordinates for frame
+    points2 = [x2;y2];
+    points4 = [x4,y4];
 end
